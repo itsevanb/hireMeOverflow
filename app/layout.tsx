80947da38d5b07411168af47565b1559e053245d
Evan <116227653+itsevanb@users.noreply.github.com>
2023-09-26 13:49:37 -0500

    implemented theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,26 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import React from 'react';
-import { Inter, Space_Grotesk } from 'next/font/google';
+import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter, Space_Grotesk } from 'next/font/google'
 import type { Metadata } from 'next';
+
 import './globals.css';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-inter'
 })
+
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spceGrotesk'
+  variable: '--font-spaceGrotesk'
 })
  
 export const metadata: Metadata = {
   title: 'hireMeOverflow',
-  description: 'A community driven platform for asking and answering programming questions. Users can get help, share knowledge, and collaborate with developers from around the world. As well as search local job boards all in one place.',
+  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.',
   icons: {
     icon: '/assets/images/site-logo.svg'
   }
@@ -29,20 +32,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: 'primary-text-gradient hover:text-primary-500',
-      }
-    }}
-    >
       <html lang="en">
-        <h1 className="h1-bold"></h1>
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
+          <ClerkProvider
+            appearance={{
+              elements: {
+                formButtonPrimary: 'primary-gradient',
+                footerActionLink: 'primary-text-gradient hover:text-primary-500'
+              }
+            }}
+          >
+            <ThemeProvider>
+              {children}
+            </ThemeProvider>
+          </ClerkProvider>
         </body>
       </html>
-    </ClerkProvider>
   )
 }
