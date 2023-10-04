f4ebba4b0d19a310fb2b0b225ab851dc96f8c92e
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-04 14:39:26 -0500

    similar styling to left sidebar
    easier, just copy and pasted className

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">Right SideBar</section>
   )
 }
 
