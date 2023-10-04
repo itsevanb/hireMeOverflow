26b7540eba9a9854d7f6b12624b3738a5222788b
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-04 15:55:27 -0500

    maps to correct questions now
    will implement suggestion algorithm at a later time

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Link from 'next/link'
+
+const hotQuestions = [
+    { _id: 1, title: 'How do I use express as a custom server in Next.js?' },
+    { _id: 1, title: 'How do I use express as a custom server in Next.js?' },
+    { _id: 1, title: 'How do I use express as a custom server in Next.js?' },
+    { _id: 1, title: 'How do I use express as a custom server in Next.js?' },
+    { _id: 1, title: 'How do I use express as a custom server in Next.js?' },
+];
 
 const RightSidebar = () => {
+
   return (
-    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">Right SideBar</section>
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen  w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+    <div>
+        <h3 className="h3-bold text-dark200_light900">
+            Top Questions</h3>
+            <div className="mt-7 flex w-full flex-col gap-[30px]">
+                {hotQuestions.map((question) => (
+                <Link
+                href={`/questions/${question._id}`}
+                key={question._id}
+                className="flex cursor-pointer items-center justify-between gap-7">
+                    <p className="body-medium text-dark500_light700">{question.title}</p>
+                </Link>
+                ))}
+            </div>
+    </div>
+    
+    <div className="mt-16">
+        test2
+    </div>
+    </section>
   )
 }
 
