3c025a99ef8641f486b5b9a4186fdf636d58bda3
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-09 17:56:38 -0500

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
           ))
           : <NoResult 
             title="There’s no question to show"
-            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            description="Be the first to break the silence! Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved!"
             link="/ask-question"
             linkTitle="Ask a Question"
           />}
