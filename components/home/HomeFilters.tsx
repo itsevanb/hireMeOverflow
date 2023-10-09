4ca6f7280f1c25b37c361f51dcd65ade71edbfc1
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-09 14:54:34 -0500

    Update HomeFilters.tsx

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -13,7 +13,7 @@ const HomeFilters = () => {
         <Button key={item.value} onClick={() => {}}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value 
           ? 'bg-primary-100 text-primary-500' 
-          : 'bg-light-800 text-light-500'
+          : 'bg-light-800 text-light-500 hover:bg-light-900 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-light-900'
         }`}
         >
           {item.name}
