dcb0510e12caa0b0fe58e3f98d32bd682f2d5872
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-12 19:53:45 -0500

    'ask question' page success
    ask-question forms have been properly set up and questions get added to mongoDB successfully
    MongoDB success

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,25 @@
 import Questions from '@/components/forms/Questions'
+import { getUserById } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  // const { userId } = auth();
+  const userId = 'clerk12345'
+
+  if (!userId) redirect('/login');
+
+  const mongoUser = await getUserById({ userId });
+
+  console.log(mongoUser);
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
       <div className="mt-9">
-        <Questions />
+        <Questions mongoUserId={JSON.stringify(mongoUser._id)} />
       </div>
     </div>
   )
