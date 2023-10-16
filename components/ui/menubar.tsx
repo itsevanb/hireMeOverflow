b7cde238d573b03b53beb22fa845186e89e6b247
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-16 18:38:33 -0500

    Update menubar.tsx

--- components/ui/menubar.tsx
+++ components/ui/menubar.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+// @ts-nocheck
 import * as React from "react"
 import * as MenubarPrimitive from "@radix-ui/react-menubar"
 import { Check, ChevronRight, Circle } from "lucide-react"
