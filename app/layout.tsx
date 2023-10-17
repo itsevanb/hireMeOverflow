2ac3424c922c0b559c566c5b7e0bb9eca02a620b
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-17 17:26:09 -0500

    Update layout.tsx
    tried to insert metadeta when shared via texts or twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next';
 
 import './globals.css';
 import { ThemeProvider } from '@/context/ThemeProvider';
+import Head from 'next/head';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -27,6 +28,7 @@ export const metadata: Metadata = {
   }
 }
  
+
 export default function RootLayout({
   children,
 }: {
@@ -34,6 +36,22 @@ export default function RootLayout({
 }) {
   return (
       <html lang="en">
+        <Head>
+          <title>hireMeOverflow</title>
+          <meta name="description" content="A community-driven platform for asking and answering programming questions." />
+          {/* Open Graph Meta Tags */}
+          <meta property="og:title" content="hireMeOverflow" />
+          <meta property="og:description" content="A community-driven platform for asking and answering programming questions." />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="Your Website URL here" />
+          <meta property="og:image" content="/path/to/your/image.jpg" />
+
+          {/* Twitter Card Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="hireMeOverflow" />
+          <meta name="twitter:description" content="A community-driven platform for asking and answering programming questions." />
+          <meta name="twitter:image" content="/path/to/your/image.jpg" />
+        </Head>
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <ClerkProvider
             appearance={{
