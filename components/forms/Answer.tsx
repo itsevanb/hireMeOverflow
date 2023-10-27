c4f3b76cf5f18bbb3484b8680fea67dc834e02f1
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-27 12:57:08 -0500

    debugging
    questions can now get answered properly

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -10,8 +10,17 @@ import { Editor } from '@tinymce/tinymce-react'
 import { useTheme } from '@/context/ThemeProvider'
 import { Button } from '../ui/button'
 import Image from 'next/image'
+import { createAnswer } from '@/lib/actions/answer.action'
+import { usePathname } from 'next/navigation'
 
-const Answer = () => {
+interface Props {
+  question: string;
+  questionId: string;
+  authorId: string;
+}
+
+const Answer = ({ question, questionId, authorId }: Props) => {
+  const pathname = usePathname();
   const [isSubmitting, setIsSubmitting] = useState(false)
   const { mode } = useTheme()
   const editorRef = useRef(null)
@@ -21,7 +30,29 @@ const Answer = () => {
   }
 })
 
-const handleCreateAnswer = () => {}
+const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
+  setIsSubmitting(true)
+
+  try {
+    await createAnswer({
+      content: values.answer,
+      author: JSON.parse(authorId),
+      question: JSON.parse(questionId),
+      path: pathname,
+    })
+    form.reset();
+
+    if(editorRef.current) {
+      const editor = editorRef.current as any;
+      editor.setContent('');
+    }
+
+  } catch (error) {
+    console.log(error)
+  } finally {
+    setIsSubmitting(false)
+  }
+}
 
   return (
     <div>
@@ -86,7 +117,7 @@ const handleCreateAnswer = () => {}
 
           <div className="flex justify-end">
             <Button
-              type="button"
+              type="submit"
               className="primary-gradient w-fit text-white"
               disabled={isSubmitting}
               >
