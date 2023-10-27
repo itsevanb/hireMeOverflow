086467b132c6d6b4412d4d4da768b303824220bd
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-26 21:12:28 -0500

    setting up answer forms
    utilizing shadcn docs

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,8 +1,22 @@
+"use client"
+
 import React from 'react'
+import { Form } from '../ui/form'
+import { useForm } from 'react-hook-form'
+import { AnswerSchema } from '@/lib/validations'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const Answer = () => {
+  const form = useForm<z.infer<typeof AnswerSchema>>({ resolver: zodResolver(AnswerSchema), 
+  defaultValues:{
+    answer: ''
+  }
+})
+
   return (
-    <div>Answer</div>
+    <Form>
+    </Form>
   )
 }
 
