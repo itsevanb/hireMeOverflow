c93eb398e0fe50749d7ab1c741286fc8f05bdf9e
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-28 13:30:49 -0500

    questions can now be responded too
    now onto votes

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from '@/components/shared/AllAnswers';
 import Metric from '@/components/shared/Metric';
 import ParseHTML from '@/components/shared/ParseHTML';
 import RenderTag from '@/components/shared/RenderTag';
+import Votes from '@/components/shared/Votes';
 import { getQuestionById } from '@/lib/actions/question.action';
 import { getUserById } from '@/lib/actions/user.action';
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
@@ -40,7 +41,7 @@ const Page = async ({ params, searchParams }) => {
         </p>
         </Link>
         <div className="flex justify-end">
-          VOTING
+          <Votes />
         </div>
       </div>
       <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
