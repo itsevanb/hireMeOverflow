15446d19040df6f17dabfa3fbfd2fff315fe95ef
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-29 18:40:01 -0500

    starting upvote/downvote functionality
    upvotes

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+"use client"
 
-const Votes = () => {
+import { formatAndDivideNumber } from "@/lib/utils";
+import Image from "next/image";
+
+interface Props {
+    type: string;
+    itemId: string;
+    userId: string;
+    upvotes: number;
+    hasupVoted: boolean;
+    downvotes: number;
+    hasdownVoted: boolean;
+    hasSaved?: boolean;
+}
+
+const Votes = ({
+    type,
+    itemId,
+    userId,
+    upvotes,
+    hasupVoted,
+    downvotes,
+    hasdownVoted,
+    hasSaved,
+}: Props) => {
+
   return (
-    <div>Votes</div>
+    <div className="flex gap-5">
+        <div className="flex-center gap-2.5">
+            <div className="flex-center gap-1.5">
+                <Image 
+                src={hasupVoted ? '/assets/icons/upvoted.svg' : '/assets/icons/upvote.svg'}
+                width={18}
+                height={18}
+                alt="upvote"
+                className="cursor-pointer"
+                onClick={() => {}}
+                />
+                <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+                    <p className="subtle-medium text-dark400_light900">
+                        {formatAndDivideNumber(upvotes)}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
