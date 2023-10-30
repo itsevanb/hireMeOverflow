9bed977cc47fa02a0da9bdb93a9d5482f478d895
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-30 11:39:16 -0500

    Update AllAnswers.tsx
    calling votes from all answers components
    similar code slight modifications

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { getTimestamp } from '@/lib/utils';
 import ParseHTML from './ParseHTML';
+import Votes from './Votes';
 
 interface Props {
     questionId: string;
@@ -55,7 +56,15 @@ const AllAnswers = async ({ questionId, userId, totalAnswers, page, filter }: Pr
                                 </div>
                             </Link>
                             <div className="flex justify-end">
-                                VOTING
+                            <Votes
+                                type="Answer"
+                                itemId={JSON.stringify(answer._id)} 
+                                userId={JSON.stringify(userId)}
+                                upvotes={answer.upvotes.length}
+                                hasupVoted={answer.upvotes.includes(userId)}
+                                downvotes={answer.downvotes.length}
+                                hasdownVoted={answer.downvotes.includes(userId)}
+                            />
                             </div>
                         </div>
                     </div>
