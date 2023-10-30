094341582480953a3068592d1a6f16fd332cd43a
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-30 11:12:50 -0500

    voting
    incorporating voting system, that saves to mongoDB

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,13 @@
 "use client"
 
+import { downvoteAnswer, upvoteAnswer } from "@/lib/actions/answer.action";
+import { viewQuestion } from "@/lib/actions/interaction.action";
+import { downvoteQuestion, upvoteQuestion } from "@/lib/actions/question.action";
+import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 interface Props {
     type: string;
@@ -24,10 +30,75 @@ const Votes = ({
     hasdownVoted,
     hasSaved,
 }: Props) => {
+    const pathname = usePathname();
+    const router = useRouter();
 
-    const handleSave = () => {}
-
-    const handleVote = (action: string) => {}
+    const handleSave = async () => {
+        await toggleSaveQuestion({
+          userId: JSON.parse(userId),
+          questionId: JSON.parse(itemId),
+          path: pathname,
+        })
+      }
+    
+      const handleVote = async (action: string) => {
+        if(!userId) {
+          return;
+        }
+    
+        if(action === 'upvote') {
+          if(type === 'Question') {
+            await upvoteQuestion({ 
+              questionId: JSON.parse(itemId),
+              userId: JSON.parse(userId),
+              hasupVoted,
+              hasdownVoted,
+              path: pathname,
+            })
+          } else if(type === 'Answer') {
+            await upvoteAnswer({ 
+              answerId: JSON.parse(itemId),
+              userId: JSON.parse(userId),
+              hasupVoted,
+              hasdownVoted,
+              path: pathname,
+            })
+          }
+    
+          // todo: show a toast
+          return;
+        }
+    
+        if(action === 'downvote') {
+          if(type === 'Question') {
+            await downvoteQuestion({ 
+              questionId: JSON.parse(itemId),
+              userId: JSON.parse(userId),
+              hasupVoted,
+              hasdownVoted,
+              path: pathname,
+            })
+          } else if(type === 'Answer') {
+            await downvoteAnswer({ 
+              answerId: JSON.parse(itemId),
+              userId: JSON.parse(userId),
+              hasupVoted,
+              hasdownVoted,
+              path: pathname,
+            })
+          }
+    
+          // todo: show a toast
+          
+        }
+      }
+    
+      useEffect(() => {
+        viewQuestion({
+          questionId: JSON.parse(itemId),
+          userId: userId ? JSON.parse(userId) : undefined,
+        })
+      }, [itemId, userId, pathname, router]);
 
   return (
     <div className="flex gap-5">
