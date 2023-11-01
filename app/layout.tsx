c079ccd49e8a4e5ead7bd360e592662556e7aab8
Evan <116227653+itsevanb@users.noreply.github.com>
2023-10-31 21:36:49 -0500

    Update layout.tsx
    cleaning

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import '../styles/prism.css';
 import { ThemeProvider } from '@/context/ThemeProvider';
-import Head from 'next/head';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -38,22 +37,6 @@ export default function RootLayout({
 }) {
   return (
       <html lang="en">
-        <Head>
-          <title>hireMeOverflow</title>
-          <meta name="description" content="A community-driven platform for asking and answering programming questions." />
-          {/* Open Graph Meta Tags */}
-          <meta property="og:title" content="hireMeOverflow" />
-          <meta property="og:description" content="A community-driven platform for asking and answering programming questions." />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="Your Website URL here" />
-          <meta property="og:image" content="/building/proj3.png" />
-
-          {/* Twitter Card Meta Tags */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="hireMeOverflow" />
-          <meta name="twitter:description" content="A community-driven platform for asking and answering programming questions." />
-          <meta name="twitter:image" content="/building/proj3.png" />
-        </Head>
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <ClerkProvider
             appearance={{
