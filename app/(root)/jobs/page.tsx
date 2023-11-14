d13b22292ac7f6ac6e4e8579d821ba388e75179c
Evan <116227653+itsevanb@users.noreply.github.com>
2023-11-14 15:10:46 -0600

    updated jobs page

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>COMING SOON 😃<br></br>
+        <br></br>
+        Finding a job is hard. Finding a job in tech is even harder. We're here to help. We're building a platform to help you find your next job in tech. We're still in the early stages of development, but we're working hard to get this ready for you. If you're interested in being a beta tester, please sign up below. We'll be in touch soon!
+    </div>
+  )
+}
+
+export default page
