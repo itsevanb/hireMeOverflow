d6bd5b61f61283591b1723bda2cdf918eed8c084
Evan <116227653+itsevanb@users.noreply.github.com>
2023-11-15 12:20:26 -0600

    Update page.tsx
    no team here, just I

--- app/(root)/jobs/page.tsx
+++ app/(root)/jobs/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div>COMING SOON 😃<br></br>
         <br></br>
-        Finding a job is hard. Finding a job in tech is even harder. We're here to help. We're building a platform to help you find your next job in tech. We're still in the early stages of development, but we're working hard to get this ready for you. If you're interested in being a beta tester, please sign up below. We'll be in touch soon!
+        Finding a job is hard. Finding a job in tech is even harder. I am here to help. I am building a platform to help you find your next job in tech. We're still in the early stages of development, but I am working hard to get this ready for you. If you're interested in being a beta tester, please sign up below. I'll be in touch soon!
     </div>
   )
 }
