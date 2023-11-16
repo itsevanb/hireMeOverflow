43627fb661df148d9be99f92543da6692f555df5
Evan <116227653+itsevanb@users.noreply.github.com>
2023-11-15 20:08:01 -0600

    Update route.ts
    changing gpt models to see if this works in deployment

--- app/api/chatgpt/route.ts
+++ app/api/chatgpt/route.ts
@@ -11,7 +11,7 @@ export const POST = async (request: Request) => {
                 Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
             },
             body: JSON.stringify({
-                model: 'gpt-4',
+                model: 'gpt-3.5-turbo',
                 messages: [
                     {
                         role: 'system',
