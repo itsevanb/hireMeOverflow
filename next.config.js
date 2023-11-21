a3e4dcb6f62a656e44115a02a2c7b4a7b508247c
Evan <116227653+itsevanb@users.noreply.github.com>
2023-11-21 16:09:04 -0600

    updating to next14
    please work
    made own d.ts file for clerk, that implies type 'any'
    getting next errors from 13 due to experimental properties I was using before updating to next14, lets pray

--- next.config.js
+++ next.config.js
@@ -1,22 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-      mdxRs: true,
-      serverComponentsExternalPackages: ['mongoose']
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '*'
-        }, 
-        {
-          protocol: 'http',
-          hostname: '*'
-        }, 
-      ]
-    }
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    mdxRs: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
